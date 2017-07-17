fdecf1e0ebd5e2baf33e439cc708da4959b9d9b1
martandatrey <martandatrey@gmail.com>
2017-07-17 19:57:44 +0530

    added firebase: json and dependencies

--- app/src/main/java/com/pimpmyapp/collegeapp/RegisterActivity.java
+++ app/src/main/java/com/pimpmyapp/collegeapp/RegisterActivity.java
@@ -2,12 +2,28 @@ package com.pimpmyapp.collegeapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    EditText nameET ,emailET, passwordET,rollnoET, phnoET, cpasswordET;
+    Spinner branch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        init();
+
+    }
+
+    private void init() {
+        nameET = (EditText) findViewById(R.id.nameET);
+        emailET = (EditText) findViewById(R.id.emailET);
+        passwordET = (EditText) findViewById(R.id.passET);
+        cpasswordET = (EditText) findViewById(R.id.cpassET);
+        phnoET = (EditText) findViewById(R.id.phnoET);
+        rollnoET = (EditText) findViewById(R.id.rollNoET);
+        branch = (Spinner) findViewById(R.id.branch);
     }
 }
