43dc7543adea39fe4ee55d18d5e094fd1c3bf780
Ajay <choudhary.asaharan@gmail.com>
2017-07-17 22:55:11 +0530

    added loginactivity

--- app/src/main/java/com/pimpmyapp/collegeapp/activity/LoginActivity.java
+++ app/src/main/java/com/pimpmyapp/collegeapp/activity/LoginActivity.java
@@ -1,17 +1,63 @@
 package com.pimpmyapp.collegeapp.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.pimpmyapp.collegeapp.R;
 
 public class LoginActivity extends AppCompatActivity {
 
 
+    EditText loginId,loginpass;
+    Button loginBtn;
+    TextView registerTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        intit();
+        methodListener();
+    }
+
+    private void intit() {
+
+        loginId = (EditText) findViewById(R.id.loginId);
+        loginpass = (EditText) findViewById(R.id.loginPass);
+        loginBtn = (Button) findViewById(R.id.loginBtn);
+        registerTextView = (TextView) findViewById(R.id.registerTextView);
+    }
+
+    private void methodListener() {
+
+        registerTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i  = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                login();
+            }
+        });
+    }
+
+    private void login() {
+
+        String username = loginId.getText().toString();
+        String password = loginpass.getText().toString();
+        
     }
 }
