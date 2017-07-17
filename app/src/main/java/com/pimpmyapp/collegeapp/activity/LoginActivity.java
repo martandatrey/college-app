18c56048721f55a685b1db0d0e2edd22b90dee11
Ajay <choudhary.asaharan@gmail.com>
2017-07-17 22:42:53 +0530

    added drawable

--- app/src/main/java/com/pimpmyapp/collegeapp/activity/LoginActivity.java
+++ app/src/main/java/com/pimpmyapp/collegeapp/activity/LoginActivity.java
@@ -1,11 +1,13 @@
 package com.pimpmyapp.collegeapp.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.pimpmyapp.collegeapp.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
